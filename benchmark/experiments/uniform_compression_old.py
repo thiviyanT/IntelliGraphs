530143bde10a9f21661285090ac42c2fac567120
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2024-08-07 14:37:49 +0200

    Rename files

--- benchmark/experiments/uniform_compression_old.py
+++ benchmark/experiments/uniform_compression_old.py
@@ -1,16 +1,20 @@
 from _context import models
+from scipy.special import gammaln
 from tqdm import trange
 import torch
 import math
 
 
-def compute_bits_for_synthetic_data(dataset):
+def compute_bits_for_synthetic_data(dataset, verbose=False, include_null_entity=False):
     """ Estimate the compression bits for storing graphs sampled using uniform distribution """
 
     train, val, test, (e2i, i2e), (r2i, i2r), _, _ = models.load_data(dataset, padding=True)
 
-    # TODO: Peter, the concept of null entity is only True for the VAE. The KGE models do not use it
-    num_entities = len(e2i) + 1  # Add one for null entities
+    num_entities = len(e2i)
+    # For the IntelliGraphs baseline, we can ignore it because padding is not used
+    if include_null_entity:
+        num_entities += 1
+
     num_relations = len(i2r)
     num_edges = train.size(1)
 
@@ -21,18 +25,21 @@ def compute_bits_for_synthetic_data(dataset):
     unique_nodes = torch.unique(concatenated)
     num_nodes = len(unique_nodes)
 
-    print(len(e2i), 'entities')
-    print(len(i2r), 'relations')
-    print(train.size(0), 'training triples')
-    print(test.size(0), 'test triples')
-    print(train.size(0) + test.size(0), 'total triples')
-    print(num_edges, 'edges')
-    print(num_nodes, 'nodes')
+    if verbose:
+        print(len(e2i), 'entities')
+        print(len(i2r), 'relations')
+        print(train.size(0), 'training triples')
+        print(test.size(0), 'test triples')
+        print(train.size(0) + test.size(0), 'total triples')
+        print(num_edges, 'edges')
+        print(num_nodes, 'nodes')
 
     # Model assumes that self-loops are not allowed and edge independence
     num_possible_edges = ((num_nodes)**2-num_nodes) * num_relations
-    bits_p_s_given_e = math.log2(num_possible_edges) * num_relations
+    bits_p_s_given_e = math.log2(num_possible_edges) * num_edges
+
     bits_p_e = math.log2(num_entities) * num_nodes
+    compression_bits = bits_p_s_given_e + bits_p_e
 
     print(f"Dataset: {dataset}")
     print(f"\tNumber of bits for p(S|E):", round(bits_p_s_given_e, 2))
@@ -40,26 +47,29 @@ def compute_bits_for_synthetic_data(dataset):
     print(f"\tTotal number of bits:", round(bits_p_e + bits_p_s_given_e, 2))
     print(f"\n\n")
 
+    return bits_p_s_given_e, bits_p_e, compression_bits
 
-def compute_bits_for_wikidata_data(dataset):
+
+def compute_bits_for_wikidata_data(dataset, verbose=False):
     """ Estimate the compression bits for storing graphs sampled using uniform distribution """
 
     train, val, test, (e2i, i2e), (r2i, i2r), (min_edges, max_edges), (min_nodes, max_nodes) = (
-        models.load_data(dataset, padding=True))
+        models.load_data(dataset, padding=True))  # TODO: Look into padding
 
-    # TODO: Peter, do we iterate through all the splits or just train and validation?
-    data = torch.cat([train, val], dim=0)
+    data = test
 
-    # TODO: Peter, the concept of null entity is only True for the VAE. The KGE models do not use it
+    # TODO: For the IntelliGraphs baseline, we can ignore it
+    # TODO: For the VAE paper, we should include it
     num_entities = len(e2i) + 1  # Add one for null entities
     num_relations = len(i2r)
     num_graphs = len(data)
 
-    print(len(e2i), 'entities')
-    print(len(i2r), 'relations')
-    print(data.size(0), 'training triples')
-    print(test.size(0), 'test triples')
-    print(data.size(0) + test.size(0), 'total triples')
+    if verbose:
+        print(len(e2i), 'entities')
+        print(len(i2r), 'relations')
+        print(data.size(0), 'training triples')
+        print(test.size(0), 'test triples')
+        print(data.size(0) + test.size(0), 'total triples')
 
     bits_p_e, bits_p_s_given_e, compression_bits = 0, 0, 0
 
@@ -79,11 +89,14 @@ def compute_bits_for_wikidata_data(dataset):
         num_nodes = len(unique_nodes)
         num_edges = graph.size(0)
 
-        _bits_p_e += math.log2(max_nodes)
+        _bits_p_e += math.log2(max_nodes)  # Explain in the paper that for simplicity we take the maximum nodes from the data (train, val, test)
         _bits_p_e += math.log2(num_entities) * num_nodes
+        # TODO: Use choose operator
 
         _bits_p_s_given_e += math.log2(max_edges)
         assert num_edges <= max_edges
+        num_possible_edges = ((num_nodes)**2-num_nodes) * num_relations
+        _bits_p_s_given_e += math.log2(num_possible_edges) * num_edges  # TODO: Use choose operator - nice to have
 
         _compression_bits = _bits_p_e + _bits_p_s_given_e
 
@@ -96,6 +109,8 @@ def compute_bits_for_wikidata_data(dataset):
     print(f"\tAverage Compression Cost for  p(E):", round(bits_p_e/num_graphs, 2))
     print(f"\tAverage Compression Cost:", round(compression_bits/num_graphs, 2))
     print(f"\n\n")
+
+    return bits_p_s_given_e, bits_p_e, compression_bits
 
 
 if __name__ == "__main__":
