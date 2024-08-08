21484aef0c64a48cf6b05241b96c5ea27e39d9b3
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2024-08-08 14:08:16 +0200

    Rename d to get_device()

--- benchmark/experiments/util/__init__.py
+++ benchmark/experiments/util/__init__.py
@@ -1,4 +1,1 @@
-from .util import mask_, d, here, contains_nan, adj, adj_triples, adj_triples_tensor, sum_sparse, contains_inf, contains_nan, \
-      logsoftmax, simple_normalize, batchmm, spmm, tic, toc, schedule, Lambda, Debug, block_diag, prod, batch, \
-      get_slug, eval, eval_simple, truedicts, initialize, compute_entity_frequency, read_config
-
+from .util import *
