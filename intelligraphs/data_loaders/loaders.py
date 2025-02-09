30e86c1a5eea92b0e05262e7cc37335c92094dbf
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2025-02-09 16:59:24 +0100

    Remove download and extraction functions from DataLoader

--- intelligraphs/data_loaders/loaders.py
+++ intelligraphs/data_loaders/loaders.py
@@ -11,6 +11,8 @@ from intelligraphs.utils import (
     create_mapping,
 )
 
+from intelligraphs.data_loaders import DatasetDownloader
+
 
 class CustomDataset(Dataset):
     """ Custom dataset class. """
@@ -39,77 +41,9 @@ class DataLoader:
         self.dataset_name = dataset_name
         self.base_dir = '.data'
 
-        # TODO: Replace with Zenodo links
-        self.download_links = {
-            'syn-paths': 'https://www.dropbox.com/s/kp1xp2rbieib4gl/syn-paths.zip?dl=1',
-            'syn-tipr': 'https://www.dropbox.com/s/wgm2yr7h8dhcj52/syn-tipr.zip?dl=1',
-            'syn-types': 'https://www.dropbox.com/s/yx7vrvsxme53xce/syn-types.zip?dl=1',
-            'wd-articles': 'https://www.dropbox.com/s/37etzy2pkix84o8/wd-articles.zip?dl=1',
-            'wd-movies': 'https://www.dropbox.com/s/gavyilqy1kb750f/wd-movies.zip?dl=1'
-        }
-
-        # Download dataset if not exists
-        self.download_data()
-
-    def create_data_folder(self):
-        """
-        Create directory for storing data, if not exists.
-
-        Returns:
-            None
-
-        """
-        if not os.path.exists(self.base_dir):
-            os.makedirs(self.base_dir)
-            print(f"Created '{self.base_dir}' folder.")
-
-    def download_data(self):
-        """
-        Download dataset if they do not already exist locally.
-
-        Returns:
-            None
-        """
-        dataset_folder = self.dataset_name
-        dataset_folder_path = os.path.join(self.base_dir, dataset_folder)
-        dataset_zip = self.dataset_name + '.zip'
-        dataset_zip_path = os.path.join(self.base_dir, dataset_zip)
-
-        self.create_data_folder()
-
-        if not os.path.exists(dataset_folder_path):
-            print(f'Dataset {self.dataset_name} not found. Downloading...')
-
-            download_link = self.download_links.get(self.dataset_name)
-            if download_link is None:
-                print(f'Dataset {self.dataset_name} is not available.')
-                return
-
-            os.makedirs(self.base_dir, exist_ok=True)
-            urllib.request.urlretrieve(download_link, dataset_zip_path)
-            print(f'Dataset {self.dataset_name} downloaded successfully.')
-
-            self.extract_dataset(dataset_zip_path)
-
-            os.remove(dataset_zip_path)
-            print(f"Removed '{dataset_zip_path}' file.")
-
-    def extract_dataset(self, dataset_path):
-        """
-        Unzip dataset.
-
-        Args:
-            dataset_path (str): Path to dataset zip file.
-
-        Returns:
-            None
-        """
-        print(f'Extracting {self.dataset_name} dataset...')
-
-        with zipfile.ZipFile(dataset_path, 'r') as zip_ref:
-            zip_ref.extractall(self.base_dir)
-
-        print(f'{self.dataset_name} dataset extracted successfully.')
+        # Download data if not exists
+        self.downloader = DatasetDownloader(download_dir=self.base_dir)
+        self.downloader.download_and_verify_all()
 
     def load_torch(self, batch_size=32, padding=True, shuffle_train=False, shuffle_valid=False, shuffle_test=False,):
         """
