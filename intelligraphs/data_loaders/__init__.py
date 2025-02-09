22a8992b4efc00722346ebbf5cb2d8d2d5208575
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2025-02-09 15:24:00 +0100

    Update module imports

--- intelligraphs/data_loaders/__init__.py
+++ intelligraphs/data_loaders/__init__.py
@@ -1,11 +1,13 @@
 from intelligraphs.data_loaders.download import DatasetDownloader
-from intelligraphs.data_loaders.loaders import IntelliGraphsDataLoader
-from intelligraphs.data_loaders.loading_functions import (
+from intelligraphs.data_loaders.loaders import DataLoader
+from intelligraphs.data_loaders.utils import (
     load_strings,
     split_subgraphs,
     compute_min_max_edges,
     compute_min_max_entities,
+    load_data_files,
     load_data_as_tensor,
     load_data_as_list,
-    pad_subgraphs
+    pad_subgraphs,
+    compute_statistics,
 )
